54f5372718c28450df3c22ff946a8575162f1ab7
Roland Kovacs <zen3ger@gmail.com>
2019-04-03 17:29:36 +0200

    Initial Phase 1 skeleteon
    Add some of the phase-1 required files to the repo.
    
    TODO:
    * Subfolder Makefile and rule implementations
    * Implement application, test, etc.

---
+++ ik2220-assign-phase1-team10/application/sdn/__init__.py
@@ -0,0 +1,5 @@
+'''IK2220 SDN Application Module'''
+
+if __name__ == '__main__':
+    print('** SDN App **')
+    exit(1)
---
+++ ik2220-assign-phase1-team10/topology/__init__.py
@@ -0,0 +1,5 @@
+'''IK2220 SDN Topology Module'''
+
+if __name__ == '__main__':
+    print('** Topology **')
+    exit(1)
