b4d2093b70fad2c57d6b5c94ad0591a6013f5784
Roland Kovacs <zen3ger@gmail.com>
2019-04-05 18:35:47 +0200

    Update configuration
    Added automatic dpid generator for swtich configuration.
    Added external controller (POX)

--- ik2220-assign-phase1-team10/topology/config.py
+++ ik2220-assign-phase1-team10/topology/config.py
@@ -1,27 +1,38 @@
 ''' IK2220 SDN Phase 1 Topology Configuration '''
 
-TOPOLOGY = { "h1"   : ('h1', 'Host', '100.0.0.11/24'),
-             "h2"   : ('h2', 'Host', '100.0.0.12/24'),
-             "ds1"  : ('ds1', 'Host', '100.0.0.20/24'),
-             "ds2"  : ('ds2', 'Host', '100.0.0.21/24'),
-             "ds3"  : ('ds3', 'Host', '100.0.0.22/24'),
-             "ws1"  : ('ws1', 'Host', '100.0.0.40/24'),
-             "ws2"  : ('ws2', 'Host', '100.0.0.41/24'),
-             "ws3"  : ('ws3', 'Host', '100.0.0.42/24'),
-             "h3"   : ('h3', 'Host', '100.0.0.51/24'),
-             "h4"   : ('h4', 'Host', '100.0.0.52/24'),
-             "sw1"  : ('sw1', 'Switch', None),
-             "sw2"  : ('sw2', 'Switch', None),
-             "sw3"  : ('sw3', 'Switch', None),
-             "sw4"  : ('sw4', 'Switch', None),
-             "sw5"  : ('sw5', 'Switch', None),
-             "fw1"  : ('sw6', 'Switch', None),
-             "fw2"  : ('sw7', 'Switch', None),
-             "ib1"  : ('sw8', 'Switch', None),
-             "ib2"  : ('sw9', 'Switch', None),
-             "napt" : ('sw10', 'Switch', None),
-             "ids"  : ('sw11', 'Switch', None),
-             "insp" : ('insp', 'Host', '100.0.0.30/24')}
+class Dpid(object):
+    cnt = 1
+    def __init__(self):
+        pass
+    @staticmethod
+    def get():
+        dpid = hex(Dpid.cnt)[2:]
+        dpid = dpid.rjust(16, '0')
+        Dpid.cnt += 1
+        return dpid
+
+TOPOLOGY = {"h1"   : ('h1', 'Host', '100.0.0.11/24'),
+            "h2"   : ('h2', 'Host', '100.0.0.12/24'),
+            "ds1"  : ('ds1', 'Host', '100.0.0.20/24'),
+            "ds2"  : ('ds2', 'Host', '100.0.0.21/24'),
+            "ds3"  : ('ds3', 'Host', '100.0.0.22/24'),
+            "ws1"  : ('ws1', 'Host', '100.0.0.40/24'),
+            "ws2"  : ('ws2', 'Host', '100.0.0.41/24'),
+            "ws3"  : ('ws3', 'Host', '100.0.0.42/24'),
+            "h3"   : ('h3', 'Host', '100.0.0.51/24'),
+            "h4"   : ('h4', 'Host', '100.0.0.52/24'),
+            "sw1"  : ('sw1', 'Switch', Dpid.get()),
+            "sw2"  : ('sw2', 'Switch', Dpid.get()),
+            "sw3"  : ('sw3', 'Switch', Dpid.get()),
+            "sw4"  : ('sw4', 'Switch', Dpid.get()),
+            "sw5"  : ('sw5', 'Switch', Dpid.get()),
+            "fw1"  : ('fw1', 'Switch', Dpid.get()),
+            "fw2"  : ('fw2', 'Switch', Dpid.get()),
+            "ib1"  : ('ib1', 'Switch', Dpid.get()),
+            "ib2"  : ('ib2', 'Switch', Dpid.get()),
+            "napt" : ('napt', 'Switch', Dpid.get()),
+            "ids"  : ('ids', 'Switch', Dpid.get()),
+            "insp" : ('insp', 'Host', '100.0.0.30/24')}
 
 LINKS = {('h1', 'sw1'),
          ('h2', 'sw1'),
@@ -33,14 +44,14 @@ LINKS = {('h1', 'sw1'),
          ('ws1', 'sw4'),
          ('ws2', 'sw4'),
          ('ws3', 'sw4'),
-         ('sw1', 'sw6'),
-         ('sw6', 'sw2'),
-         ('sw2', 'sw7'),
-         ('sw2', 'sw8'),
-         ('sw2', 'sw11'),
-         ('sw11', 'insp'),
-         ('sw11', 'sw9'),
-         ('sw9', 'sw4'),
-         ('sw8', 'sw3'),
-         ('sw7', 'sw10'),
-         ('sw10', 'sw5')}
+         ('sw1', 'fw1'),
+         ('fw1', 'sw2'),
+         ('sw2', 'fw2'),
+         ('sw2', 'ib1'),
+         ('sw2', 'ids'),
+         ('ids', 'insp'),
+         ('ids', 'ib2'),
+         ('ib2', 'sw4'),
+         ('ib1', 'sw3'),
+         ('fw2', 'napt'),
+         ('napt', 'sw5')}
--- ik2220-assign-phase1-team10/topology/topology.py
+++ ik2220-assign-phase1-team10/topology/topology.py
@@ -3,6 +3,8 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Switch
+from mininet.node import RemoteController
+from mininet.node import OVSSwitch
 from mininet.cli import CLI
 
 from config import TOPOLOGY
@@ -13,20 +15,26 @@ class Topology(Topo):
 
     def __init__(self):
         Topo.__init__(self)
-        self.machines = dict()
+        self.machines = {}
 
-        for (name, typ, addr) in TOPOLOGY.values():
+        for (name, typ, ident) in TOPOLOGY.values():
             if typ == 'Host':
                 self.machines[name] = self.addHost(name)
             if typ == 'Switch':
-                self.machines[name] = self.addSwitch(name)
+                self.machines[name] = self.addSwitch(name, dpid=ident)
 
         for (node1, node2) in LINKS:
             self.addLink(self.machines[node1], self.machines[node2])
-        print('INIT DONE')
 
 if __name__ == '__main__':
     topo = Topology()
-    net = Mininet(topo=topo)
+    ctrl = RemoteController('c0', ip='127.0.0.1', port=6633)
+    net = Mininet(topo=topo,
+                  switch=OVSSwitch,
+                  controller=ctrl,
+                  autoSetMacs=True,
+                  autoStaticArp=True,
+                  build=True,
+                  cleanup=True)
     net.start()
     CLI(net)
