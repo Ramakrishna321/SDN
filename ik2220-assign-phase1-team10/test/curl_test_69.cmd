c634a2f32d7fd3308767a868e5672d959a124c89
rk <c.v.ramakrishna321@gmail.com>
2019-04-18 20:16:03 +0200

    Curl @ 69

--- ik2220-assign-phase1-team10/test/curl_test_69.cmd
+++ ik2220-assign-phase1-team10/test/curl_test_69.cmd
@@ -2,6 +2,6 @@ h2 curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.40:69 --m
 h4 curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.41:69 --max-time 5
 insp curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.42:69 --max-time 5
 
-h2 curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.40 -p 69 --max-time 5
-h4 curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.41 -p 69 --max-time 5
-insp curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.42 -p 69 --max-time 5
+h2 curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.40:69 --max-time 5
+h4 curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.41:69 --max-time 5
+insp curl -w '\n %{local_ip} to %{remote_ip} : %{remote_port}\n' 100.0.0.42:69 --max-time 5
